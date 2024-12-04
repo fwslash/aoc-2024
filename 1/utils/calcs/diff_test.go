491fa0c38930be7f8fedd041e4ca5c6c4e1eb314
Karlo Bartolic <karlo.bartolic@adcubum.com>
2024-12-04 20:50:18 +0100

    [1] - Added ArrDiff test

--- 1/utils/calcs/diff_test.go
+++ 1/utils/calcs/diff_test.go
@@ -1,7 +1,15 @@
 package calcs
 
-import "Testing"
+import "testing"
 
 func TestArrDiff(t *testing.T) {
+  arr1 := []int{1,2,3}
+  arr2 := []int{2,3,4}
 
+  got := ArrDiff(arr1, arr2)
+  want := 3
+
+  if got != want {
+    t.Errorf("Arr Diff did not return correct number. Got: %d, Want; %d ", got, want)
+  }
 }
